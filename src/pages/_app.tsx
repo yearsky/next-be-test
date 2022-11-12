dc5bcc38ef07b75ec96ff318b5fc847044fb0de1
Yearsky <43109148+yearsky@users.noreply.github.com>
2022-11-12 21:45:09 +0700

    m

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ export default withTRPC<ServerRouter>({
   config({ ctx }) {
     const url = process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : "http://localhost:3000/api/trpc";
+      : "http://localhost:3001/api/trpc";
 
     return { url };
   },
